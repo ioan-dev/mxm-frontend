f53ab3b399e60737895aa5d921fbde705c76c09e
ioan-dev <82202528+tumanoviv@users.noreply.github.com>
2026-04-09 16:27:36 +0300

    Docker files add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node'
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
